170cf160a2bda00fbd7de37ee9b2e04172f6aab7
hp <hp@localhost.localdomain>
2022-02-19 16:33:45 -0600

    data

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,21 +1,22 @@
 /**
  * @description
  */
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PageHome from "pages/Home";
 import Quiz from "pages/Quiz";
 import Lecture from "pages/Lecture";
 import Module from "pages/Module";
-
+import test from "pages/Home"
+console.log("test",test)
 const Router = function () {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<PageHome />} />
-        <Route path="/module" element={<Module />} />
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/lecture" element={<Lecture />} />
-        <Route path="/project" element={<Module />} />
+        <Route path="/module" element={<Module />} />
       </Routes>
     </BrowserRouter>
   )
